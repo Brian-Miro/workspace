57fc922a613c5a713497e4df79d0b003a4a21d17
Brian-Miro <brimirvaz@alu.edu.gva.es>
2024-02-26 10:43:30 +0100

    Ej. generador contraseñas finalizado

--- Ejercicios/ejercicio_generador_contraseñas.py
+++ Ejercicios/ejercicio_generador_contraseñas.py
@@ -31,5 +31,36 @@ def cambiaTexto3(palabra):
 
 def cambiaTexto4(palabra):
     longitud_palabra = len(palabra)
-    if 
-    # Poner guiones bajos
+    if longitud_palabra <= 5:
+        temp1 = palabra[:3]
+        temp2 = palabra[3:]
+        palabra = temp1 + "_" + temp2
+        return palabra
+    else:
+        temp1 = palabra[:(longitud_palabra//4)]
+        temp2 = palabra[(longitud_palabra//4):(longitud_palabra*2//4)]
+        temp3 = palabra[(longitud_palabra*2//4):(longitud_palabra*3//4)]
+        temp4 = palabra[(longitud_palabra*3//4):]
+        palabra = temp1 + "_" + temp2 + "_" + temp3 + "_" + temp4
+        return palabra
+
+texto = input("Por favor, introduce tu palabra: ")
+numero = random.randint(1,3)
+print(f'El número aleatorio es {numero}')
+
+if numero == 1:
+    texto = cambiaTexto4(cambiaTexto3(cambiaTexto2(cambiaTexto1(texto))))
+elif numero == 2:
+    texto = cambiaTexto3(cambiaTexto1(cambiaTexto4(cambiaTexto2(texto))))
+else:
+    texto = cambiaTexto4(cambiaTexto1(cambiaTexto2(cambiaTexto3(texto))))
+
+if len(texto) > 10:
+    texto = texto[:10]
+elif len(texto) < 10:
+    while len(texto) < 10:
+        texto = texto + str(random.randint(0,9))
+else:
+    texto = texto
+
+print(f'Tu contraseña es: {texto}')
